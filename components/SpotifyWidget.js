f96c2ddfa3e0c99b8578f962c891870649d0a3a6
ollie <olliedean@kakao.com>
2021-12-25 13:13:28 +0000

    made it look less ugly on initial load

--- components/SpotifyWidget.js
+++ components/SpotifyWidget.js
@@ -1,5 +1,6 @@
 import { faSpotify } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Image from "next/image"
 import useSWR from "swr"
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
@@ -22,18 +23,20 @@ export default function SpotifyWidget() {
 			<span className="text-xs text-gray-100 font-extrabold">
 			  {now?.isPlaying ? "CURRENTLY PLAYING" : "LAST PLAYED" }
 			</span>
-			<div class="flex pt-2 items-center">
-			  <img
-				src={now?.albumImageUrl}
-				alt="currently playing spotify cover"
-				className="w-12 rounded"
+			<div className="flex pt-2 items-center">
+			  <Image
+				src={now?.albumImageUrl || "/public/no-album.png"}
+				alt=""
+				className="rounded"
+				width={48}
+				height={48}
 			  />
 			  <div className="leading-none text-white font-semibold pl-2 truncate text-sm">
 				{now?.title} <br />
-				<span class="text-gray-200 text-xs font-normal">{now?.artist}</span>
+				<span className="text-gray-200 text-xs font-normal">{now?.artist}</span>
 			  </div>
 			</div>
-			<div class="group-hover:text-white absolute top-0 right-0 mt-2 mr-2 text-gray-200">
+			<div className="group-hover:text-white absolute top-0 right-0 mt-2 mr-2 text-gray-200">
 			  <FontAwesomeIcon icon={faSpotify} size="lg" />
 			</div>
 		  </div>
