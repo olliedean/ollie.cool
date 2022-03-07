a2927c4c30f949bc8bc6de4d30a1b132fed6fc4e
ollie <olliedean@kakao.com>
2022-03-07 12:45:33 +0000

    made location open new tab

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
             <span className="text-2xl font-bold">👋 hiya, i'm ollie</span>
             <p>
               a freelance fullstack developer based in&nbsp;
-              <a href="https://www.google.com/maps/place/Lincoln/@53.2182753,-0.6255359,12z/data=!3m1!4b1!4m5!3m4!1s0x48780ccb711104fb:0x36603dcfe8c7ddc7!8m2!3d53.230688!4d-0.540579">lincoln, uk.</a>
+              <a href="https://www.google.com/maps/place/Lincoln/@53.2182753,-0.6255359,12z/data=!3m1!4b1!4m5!3m4!1s0x48780ccb711104fb:0x36603dcfe8c7ddc7!8m2!3d53.230688!4d-0.540579" target="_blank">lincoln, uk.</a>
               <br />
               <SocialBadge link="https://github.com/olliedean" hoverColor="text-gray-500" className="hover:text-gray-700">
                 <FontAwesomeIcon icon={faGithub} />
